226c9b01cbb4e5bfd942bee255b53e151d3d9e53
hamza887 <hamzabutt887>
2021-06-13 04:16:25 -0400

    reversed a string

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+const reverseastring =(rs)=>{
+      const splitstring= rs.split('');
+      const changeme=splitstring.reverse();
+      const joinarray=changeme.join('')
+      console.log(joinarray)
+}
+//console.log(splitstring);
+reverseastring('hello');
